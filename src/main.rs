126f40c806bddde8770e7b8c4b77129a1bc155d1
Hamid R. K. Pishghadam <hamidrkp@riseup.net>
2020-05-18 18:28:07 +0430

    Halfway of adding database to project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+
+use rocket_contrib::databases::diesel;
+
+#[database("sqlite_main")]
+struct DbConn(diesel::SqliteConnection);
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![index])
+        .attach(DbConn::fairing())
+        .launch();
 }
